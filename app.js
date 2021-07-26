4a7e96178dd6404db4e39fde1be1707db8b65b54
Pete5145 <peterosakpolor661@gmail.com>
2021-07-26 06:07:33 -0700

    first-commit

---
+++ app.js
@@ -0,0 +1,55 @@
+// All valid credit card numbers
+const valid1 = "4539 6779 0801 6808";
+const valid2 = "5535 7667 6875 1439";
+const valid3 = "3716 1201 9985 236";
+const valid4 = "6011 1443 4068 2905";
+const valid5 = "4539 4049 6786 9666";
+
+// All invalid credit card numbers
+const invalid1 = "4532 7787 7109 1795";
+const invalid2 = "5795 5933 9213 4643";
+const invalid3 = "3757 9608 4459 914";
+const invalid4 = "6011 1279 6177 7935";
+const invalid5 = "5382 0197 7288 3854";
+
+// Can be either valid or invalid
+const notCertain1 = "3448 0196 8305 414";
+const notCertain2 = "5466 1008 6162 0239";
+const notCertain3 = "6011 3770 2096 2656 203";
+const notCertain4 = "4929 8771 6921 7093";
+const notCertain5 = "4913 5404 6307 2523";
+
+// An array of all the arrays above
+
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, notCertain1, notCertain2, notCertain3, notCertain4, notCertain5]
+
+// Add your functions below:
+function transformStrArr(valid, inValid, notCertain){
+    valid1.split(" ");
+    valid2.split(" ");
+    valid3.split(" ");
+    valid4.split(" ");
+    valid5.split(" ");
+
+    invalid1.split(" ");
+    invalid2.split(" ");
+    invalid3.split(" ");
+    invalid4.split(" ");
+    invalid5.split(" ");
+
+    notCertain1.split(" ");
+    notCertain2.split(" ");
+    notCertain3.split(" ");
+    notCertain4.split(" ");
+    notCertain5.split(" ");
+}
+
+function validateCard(array) {
+    if (typeof batch == number) {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+
